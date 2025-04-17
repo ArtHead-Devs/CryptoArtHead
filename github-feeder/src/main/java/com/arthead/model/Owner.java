6f6f8660c0477d6e24ba7fc9c1c260552f71be7b
NestX10 <nestauni10@gmail.com>
2025-04-17 19:46:02 +0100

    feature: deserializador creado

---
+++ github-feeder/src/main/java/com/arthead/model/Owner.java
@@ -0,0 +1,13 @@
+package com.arthead.model;
+
+public class Owner {
+    private String login;
+
+    public Owner(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
