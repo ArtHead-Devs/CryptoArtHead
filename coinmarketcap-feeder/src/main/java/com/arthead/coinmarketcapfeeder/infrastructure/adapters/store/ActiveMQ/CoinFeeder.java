586ce1942a99dd12b4059e4c2f0f071d3cef2d68
NestX10 <nestauni10@gmail.com>
2025-05-18 00:53:09 +0100

    Refactoring realizado para los feeders

--- coinmarketcap-feeder/src/main/java/com/arthead/coinmarketcapfeeder/infrastructure/adapters/store/ActiveMQ/CoinFeeder.java
+++ coinmarketcap-feeder/src/main/java/com/arthead/coinmarketcapfeeder/infrastructure/adapters/store/ActiveMQ/CoinFeeder.java
@@ -10,31 +10,45 @@ import java.util.List;
 public class CoinFeeder {
     private final String url;
     private static final String subject = "crypto.Coins";
+    private final Gson gson = new Gson();
 
     public CoinFeeder(String url) {
         this.url = url;
     }
 
     public void sendCoins(List<Coin> coins) throws JMSException {
+        Connection connection = createAndStartConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        MessageProducer producer = setupTopicProducer(session);
+        sendMessages(coins, producer, session);
+        closeResources(connection);
+    }
+
+    private Connection createAndStartConnection() throws JMSException {
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
         connection.start();
+        return connection;
+    }
 
-        Session session = connection.createSession(false,
-                Session.AUTO_ACKNOWLEDGE);
-
+    private MessageProducer setupTopicProducer(Session session) throws JMSException {
         Destination destination = session.createTopic(subject);
+        return session.createProducer(destination);
+    }
 
-        MessageProducer producer = session.createProducer(destination);
-
-        Gson gson = new Gson();
-        for(Coin coin: coins){
-            String json = gson.toJson(coin);
-            TextMessage message = session
-                    .createTextMessage(json);
+    private void sendMessages(List<Coin> coins, MessageProducer producer, Session session) throws JMSException {
+        for (Coin coin : coins) {
+            TextMessage message = createMessageForCoin(coin, session);
             producer.send(message);
-            System.out.println("crypto.Coin printing@@ '" + message.getText() + "'");
         }
+    }
+
+    private TextMessage createMessageForCoin(Coin coin, Session session) throws JMSException {
+        String json = gson.toJson(coin);
+        return session.createTextMessage(json);
+    }
+
+    private void closeResources(Connection connection) throws JMSException {
         connection.close();
     }
 }
--- coinmarketcap-feeder/src/main/java/com/arthead/coinmarketcapfeeder/infrastructure/adapters/store/ActiveMQ/QuoteFeeder.java
+++ coinmarketcap-feeder/src/main/java/com/arthead/coinmarketcapfeeder/infrastructure/adapters/store/ActiveMQ/QuoteFeeder.java
@@ -10,31 +10,45 @@ import java.util.List;
 public class QuoteFeeder {
     private final String url;
     private static final String subject = "crypto.Quotes";
+    private final Gson gson = new Gson();
 
     public QuoteFeeder(String url) {
         this.url = url;
     }
 
     public void sendQuotes(List<Quote> quotes) throws JMSException {
+        Connection connection = createAndStartConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        MessageProducer producer = setupTopicProducer(session);
+        sendMessages(quotes, producer, session);
+        closeResources(connection);
+    }
+
+    private Connection createAndStartConnection() throws JMSException {
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
         connection.start();
+        return connection;
+    }
 
-        Session session = connection.createSession(false,
-                Session.AUTO_ACKNOWLEDGE);
-
+    private MessageProducer setupTopicProducer(Session session) throws JMSException {
         Destination destination = session.createTopic(subject);
+        return session.createProducer(destination);
+    }
 
-        MessageProducer producer = session.createProducer(destination);
-
-        Gson gson = new Gson();
-        for(Quote quote: quotes){
-            String json = gson.toJson(quote);
-            TextMessage message = session
-                    .createTextMessage(json);
+    private void sendMessages(List<Quote> quotes, MessageProducer producer, Session session) throws JMSException {
+        for (Quote quote : quotes) {
+            TextMessage message = createMessageForQuote(quote, session);
             producer.send(message);
-            System.out.println("crypto.Quotes printing@@ '" + message.getText() + "'");
         }
+    }
+
+    private TextMessage createMessageForQuote(Quote quote, Session session) throws JMSException {
+        String json = gson.toJson(quote);
+        return session.createTextMessage(json);
+    }
+
+    private void closeResources(Connection connection) throws JMSException {
         connection.close();
     }
 }
