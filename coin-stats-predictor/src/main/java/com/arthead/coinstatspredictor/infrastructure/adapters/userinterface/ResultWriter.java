176f80a3bd131601b4e39d7908a5441d13b6d3bc
Pablo <pabcablan2005@gmail.com>
2025-05-18 23:39:53 +0100

    Commit v3.0.0, correcione + ejemplo datamart

--- coin-stats-predictor/src/main/java/com/arthead/coinstatspredictor/infrastructure/adapters/userinterface/ResultWriter.java
+++ coin-stats-predictor/src/main/java/com/arthead/coinstatspredictor/infrastructure/adapters/userinterface/ResultWriter.java
@@ -2,6 +2,7 @@ package com.arthead.coinstatspredictor.infrastructure.adapters.userinterface;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.*;
 
 public class ResultWriter {
     private final String csvPath;
@@ -11,6 +12,10 @@ public class ResultWriter {
     }
 
     public void write(String content) throws IOException {
+        Path path = Paths.get(csvPath);
+        if (!Files.exists(path.getParent())) {
+            Files.createDirectories(path.getParent());
+        }
         try (FileWriter writer = new FileWriter(csvPath)) {
             writer.write(content);
         }
